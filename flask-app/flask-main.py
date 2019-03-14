572e68df2043f7239498bbcfa69a4510b30e1fec
Kevin Webb <kevinlwebb03@gmail.com>
2019-03-14 17:09:23 -0500

    Added two paths
    The added paths show capability with dictionary and passed url argument

--- flask-app/flask-main.py
+++ flask-app/flask-main.py
@@ -5,5 +5,14 @@ app = Flask(__name__)
 def index():
    return render_template(‘hello.html’)
 
+@app.route('/hello/<int:score>')
+def hello_name(score):
+   return render_template('score.html', marks = score)
+
+@app.route('/result')
+def result():
+   dict = {'phy':50,'che':60,'maths':70}
+   return render_template('result.html', result = dict)
+
 if __name__ == '__main__':
    app.run(debug = True)
